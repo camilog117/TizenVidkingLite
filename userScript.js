9371b9d8e86bd2e0fa062487fff09d10b828d575
camilog117 <c.gomezcreativo@gmail.com>
2026-03-12 11:27:37 -0500

    Add user script for Vidking Lite on Tizen TV

---
+++ userScript.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Vidking Lite para Tizen TV
+// @version      1.0.0
+// @match        https://raw.githubusercontent.com/*/*/main/index.html*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Vidking Lite cargado – modo ligero activado');
+
+    // Spoof User-Agent para evitar bloqueos
+    Object.defineProperty(navigator, 'userAgent', {
+        get: () => 'Mozilla/5.0 (SMART-TV; Linux; Tizen 7.0) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/7.0 Chrome/108.0.0.0 TV Safari/537.36'
+    });
+
+    // Forzar fullscreen automático
+    setInterval(() => {
+        const iframe = document.querySelector('iframe');
+        if (iframe && !document.fullscreenElement) {
+            iframe.requestFullscreen().catch(() => {});
+        }
+    }, 3000);
+})();
